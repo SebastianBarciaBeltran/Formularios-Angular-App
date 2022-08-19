f59040ddbd30a87d849fa050063516add9286fc9
Sebastián Barcia Beltrán <sebastianbarciabeltran@gmail.com>
2022-08-19 16:45:30 +0200

    parte de switches terminada

--- src/app/template/switches/switches.component.ts
+++ src/app/template/switches/switches.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
+  persona = {
+    genero: 'F',
+    notificaciones: false,
+  }
+
+  terminosYCondiciones: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
