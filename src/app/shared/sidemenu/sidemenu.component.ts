9dc8a02332d36719cf8e60d16173868d9b8822b6
Sebastián Barcia Beltrán <sebastianbarciabeltran@gmail.com>
2022-08-19 12:05:57 +0200

    sideMenu listo con su navegacion correspondiente

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from '../interfaces/menu.interfaces';
 
 @Component({
   selector: 'app-sidemenu',
@@ -6,6 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidemenu.component.css']
 })
 export class SidemenuComponent implements OnInit {
+
+  templateMenu: MenuItem[] = [
+    {
+      texto: 'Básicos',
+      ruta: './template/basicos'
+    },
+    {
+      texto: 'Dinámicos',
+      ruta: './template/dinamicos'
+    },
+    {
+      texto: 'Switches',
+      ruta: './template/switches'
+    }
+  ];
+
+  reactiveMenu: MenuItem[] = [
+    {
+      texto: 'Básicos',
+      ruta: './reactive/basicos'
+    },
+    {
+      texto: 'Dinámicos',
+      ruta: './reactive/dinamicos'
+    },
+    {
+      texto: 'Switches',
+      ruta: './reactive/switches'
+    }
+  ];
 
   constructor() { }
 
