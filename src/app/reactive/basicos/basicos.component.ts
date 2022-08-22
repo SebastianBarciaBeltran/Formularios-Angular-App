6ea301b4c4ce5d0d074103e3f614c22298e925a7
Sebastián Barcia Beltrán <sebastianbarciabeltran@gmail.com>
2022-08-22 17:53:55 +0200

    formBuilder implementado con sus respectivos validators, pendiente mostrar los mensajes de error condicionales

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -7,7 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
-  constructor() { }
+  // miFormulario : FormGroup = new FormGroup({
+  //   producto: new FormControl('RTX 4080ti'),
+  //   precio: new FormControl(1500),
+  //   existencias: new FormControl(5),
+  // });
+
+  miFormulario : FormGroup = this.fb.group({
+    producto: ['RTX 4080ti', [Validators.required, Validators.minLength(3)]],
+      precio: [1500, [Validators.required, Validators.min(0)]],
+      existencias: [10, [Validators.required, Validators.min(0)]],
+  });
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
