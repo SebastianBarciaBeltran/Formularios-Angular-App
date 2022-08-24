beabb5b3d7bcb77f8c35bfdcf0d25974b4b62885
Sebastián Barcia Beltrán <sebastianbarciabeltran@gmail.com>
2022-08-24 13:21:27 +0200

    formulario reactivos en switches implementado

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    genero: ['M', [Validators.required]],
+    notificiaciones: [true, [Validators.required]],
+    terminosYCondiciones: [true, [Validators.requiredTrue]],
+  });
+
+  // terminosYCondiciones: FormControl = this.fb.control(true, Validators.required);
+
+  persona = {
+    genero: 'F',
+    notificiaciones: true,
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+
+    this.miFormulario.reset({
+      ...this.persona,
+      terminosYCondiciones: false
+    });
+
+
+    // this.miFormulario.valueChanges.subscribe( form => {
+    //   delete form.terminosYCondiciones;
+    //   this.persona = form;
+    // });
+   
+    this.miFormulario.valueChanges.subscribe( ({ terminosYCondiciones, ...rest }) => {
+      // delete form.terminosYCondiciones;
+      this.persona = rest;
+    });
+
+  }
+
+
+  guardar(){
+    const formValue = { ...this.miFormulario.value };
+    delete formValue.terminosYCondiciones;
+    this.persona = formValue;
   }
 
 }
